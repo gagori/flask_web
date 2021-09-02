f1518be0ba1f36e5a8f8a4b4dd77c43f5941801f
Your Name <you@example.com>
2021-09-02 15:13:39 +0900

    flask_web complete

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, redirect, request
-from flask.signals import request_tearing_down
-from data import Articles
+from flask import Flask, render_template, redirect, request, session, url_for
+# from data import Articles
 import pymysql
 from passlib.hash import pbkdf2_sha256
+from functools import wraps
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'gangnam'
 
 db_connection = pymysql.connect(
 	    user    = 'root',
@@ -13,7 +16,29 @@ db_connection = pymysql.connect(
     	charset = 'utf8'
 )
 
-app = Flask(__name__)
+# 로그아웃상태서 삭제,편집,쓰기 기능이 안되게 하기 위해서.
+def is_logged_in(f):
+    # @데코레이터 체크기능 함수 만드는 방법
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'is_logged' in session:
+            return f(*args, **kwargs)   # 체크 통과 후 다음의 함수가 시행된다.
+        else:
+            return redirect(url_for('login'))
+    return wrap
+
+# 삭제는 admin만 할 수 있게.
+def is_admin(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if session['email'] == 'admin@gmail.com' :
+            return f(*args, **kwargs)
+        else:
+            return redirect('/articles')
+    return wrap
+
+
+
 
 @app.route('/hello')
 def hello_world():
@@ -22,12 +47,14 @@ def hello_world():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = "JUNG"
-    return render_template('index.html', data=name)
+    # print(request.headers)
+    print(len(session))      # 로그인 안되면 0, 로그인되면 다른 숫자.
+    return render_template('index.html', data=name, user=session)  # 아직 로그인 유지 check는 안하고 있음. 껍데기만 하는중.
 
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method =='GET':
-        return render_template('register.html')
+        return render_template('register.html', user=session)
     else:
         username = request.form["username2"]
         email = request.form["email2"]
@@ -51,7 +78,7 @@ def register():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login.html', user=session)
     else:
         email = request.form['email']
         password = request.form['password']
@@ -68,11 +95,24 @@ def login():
             # 이제 비밀번호 확인
             # 조회해온 데이터가 튜플이므로 튜플사용.
             # verify이용.
+            # 로그인이 되면 session이라는 dict생기게만듬.
             result =  pbkdf2_sha256.verify(password, user[3])
             if result == True:
-                return "SUCCESS"
+                #세션 딕셔너리형태로 저장함.
+                session['id']  = user[0]
+                session['username'] = user[1]
+                session['email'] = user[2]
+                session['date'] = user[4]
+                session['is_logged'] = True
+                print(session)
+                return redirect('/')
             else:
                 return redirect('login')
+
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    session.clear()
+    return redirect('/')
             
 
 
@@ -84,7 +124,7 @@ def articles():
     cursor.execute(sql)
     topics = cursor.fetchall()
     print(topics)
-    return render_template('articles.html', data = topics)
+    return render_template('articles.html', data = topics, user=session)
     
 @app.route('/detail/<ids>')  # <> params처리
 def detail(ids):
@@ -97,13 +137,14 @@ def detail(ids):
     # for data in list_data:
     #     if data['id'] == int(ids):
     #         article = data
-    return render_template('article.html', article1=topic)
+    return render_template('article.html', article1=topic, user=session)
 
 # post방식 구현, request모듈 사용
 @app.route('/add_article', methods=['GET','POST'])
+@is_logged_in
 def add_article():
     if request.method == "GET":
-        return render_template('add_article.html')
+        return render_template('add_article.html', user=session)
     else:
         title = request.form["title"]
         desc = request.form["desc"]
@@ -113,9 +154,11 @@ def add_article():
         sql = f"INSERT INTO list (title, description, author) VALUES ('{title}', '{desc}', '{author}');"
         cursor.execute(sql)
         db_connection.commit()
-        return redirect('/articles')   # /add_article 경로에서 /articles경로로 다시 돌아감.
+        return redirect('/articles' )   # /add_article 경로에서 /articles경로로 다시 돌아감.
 
 @app.route('/edit_article/<ids>', methods=['GET','POST'])
+# @ 체크함수
+@is_logged_in
 def edit_article(ids):
     if request.method == 'GET':
         cursor = db_connection.cursor()
@@ -123,7 +166,7 @@ def edit_article(ids):
         cursor.execute(sql)
         topic = cursor.fetchone()       # select 즉, 조회니까 commit없이.
         print(topic)
-        return render_template('edit_article.html', article1=topic)
+        return render_template('edit_article.html', article1=topic, user=session)
         
     else:
         title = request.form["title"]
@@ -138,6 +181,8 @@ def edit_article(ids):
 
 
 @app.route('/delete/<ids>', methods=['GET','POST'])
+@is_logged_in
+@is_admin
 def delete(ids):
     cursor =db_connection.cursor()
     sql = f'DELETE FROM list WHERE id={int(ids)};'   # DB에서 Delete Row(s)하는 쿼리문.
